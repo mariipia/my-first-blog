9aad54656f5b2cb9a1de29196fb455da62630c08
mariipia <mp.heller.10@gmail.com>
2019-12-04 16:52:52 -0300

    Cambie el HTML para la pag.

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
